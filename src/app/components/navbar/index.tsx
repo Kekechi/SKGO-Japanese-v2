f89bc4be7c8731129d5ccbcb30789ed1f78ac056
Keigo Morita <kekechipon@gmail.com>
2024-09-10 20:22:37 -0700

    Update index.tsx

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -6,44 +6,50 @@ const Navbar = () => {
     <div>
       <nav className="width-full bg-green-300">
         <div className="mx-auto max-w-screen-lg px-4 flex">
-          <h1 className="text-2xl font-bold flex-none px-4">SKGO Japanese</h1>
+          <h1 className="text-2xl font-bold flex-none p-3">SKGO Japanese</h1>
           <ul className="flex flex-auto">
-            <li className=" text-center bg-red-100 border-2 flex-auto py-4">
-              <Link href="home" className="text-xl font-bold ">
+            <li className="text-center flex-auto py-3">
+              <Link href="/" className="text-lg font-bold ">
                 Home
               </Link>
             </li>
-            <li className=" text-center bg-red-100 border-2 flex-auto py-4">
-              <Link href="language" className="text-xl font-bold">
+            <li className="text-center flex-auto py-3">
+              <Link href="/language" className="text-lg font-bold">
                 Language
               </Link>
             </li>
-            <li className=" text-center bg-red-100 border-2 flex-auto py-4">
-              <Link href="game" className="text-xl font-bold">
-                Game
+            <li className=" text-center flex-auto py-3">
+              <Link href="/games" className="text-lg font-bold">
+                Games
               </Link>
             </li>
-            <li className=" text-center bg-red-100 border-2 flex-auto py-4">
-              <Link href="" className="text-xl font-bold">
+            <li className="text-center flex-auto py-3">
+              <Link href="/presentations" className="text-lg font-bold">
                 Anime & Pop Culture
               </Link>
             </li>
-            <li className=" text-center bg-red-100 border-2 flex-auto py-4">
-              <Link href="culture" className="text-xl font-bold">
+            <li className=" text-center flex-auto py-3">
+              <Link href="/presentations" className="text-lg font-bold">
                 Japanese Culture
               </Link>
             </li>
-            <li className="text-center bg-red-100 border-2 flex-auto relative hover:bg-green-200">
-              <Link href="about" className="text-xl font-bold">
-                <div className="py-4">About Us</div>
-              </Link>
-              <ul className="fixed mt-2 p-4 bg-green-300">
-                <li className=" text-center bg-red-100 border-2 text-lg font-semibold">
-                  Mission
-                </li>
-                <li>Founders</li>
-                <li>Volunteers</li>
-              </ul>
+            <li className="group text-center flex-auto relative">
+              <div className="text-lg font-bold w-full h-full hover:bg-green-200">
+                <div className="py-3">About Us</div>
+              </div>
+              <div className="hidden group-hover:block fixed ">
+                <ul className="p-4 bg-green-300 drop-shadow mt-1">
+                  <li className="text-center text-lg font-semibold">
+                    <Link href="/about/mission">Mission</Link>
+                  </li>
+                  <li>
+                    <Link href="/about/founders">Founders</Link>
+                  </li>
+                  <li>
+                    <Link href="/about/volunteers">Volunteers</Link>
+                  </li>
+                </ul>
+              </div>
             </li>
           </ul>
         </div>
