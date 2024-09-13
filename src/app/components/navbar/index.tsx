d20a1434befaf9d272e035f7bf71b04fa025c536
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-09-13 14:58:42 -0700

    navigation style

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -7,10 +7,7 @@ const Navbar = () => {
     <nav className="width-full bg-green-300 sticky top-0 z-50">
       {/* Mobile Nav Bar */}
       <div className="md:hidden">
-        <div
-          className="h-11 w-full
-          "
-        >
+        <div className="h-11 w-full">
           <div className="fixed w-2/3 h-full bg-gray-600 -translate-x-full has-[div:focus]:-translate-x-0 transition-transform">
             {/* Menu Icon */}
             <div className="float-right translate-x-full relative">
@@ -82,47 +79,52 @@ const Navbar = () => {
         </div>
       </div>
       {/* Desktop Nav Bar */}
-      <div className="mx-auto max-w-screen-lg px-4 hidden md:flex">
-        <h1 className="text-2xl font-bold flex-none p-3">SKGO Japanese</h1>
-        <ul className="flex flex-auto">
-          <li className="text-center flex-auto py-3">
-            <Link href="/" className="text-lg font-bold ">
+      <div className="mx-auto max-w-screen-lg px-4 h-14 hidden md:flex">
+        <ul className="flex flex-auto justify-between">
+          <li className="text-center flex-auto w-min  my-auto">
+            <Link href="/" className="text-lg font-bold leading-tight">
               Home
             </Link>
           </li>
-          <li className="text-center flex-auto py-3">
-            <Link href="/language" className="text-lg font-bold">
+          <li className="text-center flex-auto w-min  my-auto">
+            <Link href="/language" className="text-lg font-bold leading-tight">
               Language
             </Link>
           </li>
-          <li className=" text-center flex-auto py-3">
-            <Link href="/games" className="text-lg font-bold">
+          <li className=" text-center flex-auto w-min  my-auto">
+            <Link href="/games" className="text-lg font-bold leading-tight">
               Games
             </Link>
           </li>
-          <li className="text-center flex-auto py-3">
-            <Link href="/presentations" className="text-lg font-bold">
+          <li className="text-center flex-auto w-min  my-auto">
+            <Link
+              href="/presentations"
+              className="text-lg font-bold leading-tight"
+            >
               Anime & Pop Culture
             </Link>
           </li>
-          <li className=" text-center flex-auto py-3">
-            <Link href="/presentations" className="text-lg font-bold">
+          <li className=" text-center flex-auto w-min  my-auto">
+            <Link
+              href="/presentations"
+              className="text-lg font-bold leading-tight"
+            >
               Japanese Culture
             </Link>
           </li>
-          <li className="group text-center flex-auto relative">
-            <div className="text-lg font-bold w-full h-full hover:bg-green-200">
-              <div className="py-3">About Us</div>
+          <li className="group text-center flex-auto w-min relative">
+            <div className="text-lg font-bold w-full h-full hover:bg-green-200 flex flex-col justify-center">
+              <div className="leading-tight">About Us</div>
             </div>
-            <div className="hidden group-hover:block fixed ">
-              <ul className="p-4 bg-green-300 drop-shadow mt-1">
-                <li className="text-center text-lg font-semibold">
+            <div className="hidden group-hover:block absolute w-full">
+              <ul className="p-auto py-4 bg-green-300 drop-shadow mt-1 rounded-lg">
+                <li className="text-center text-lg font-medium">
                   <Link href="/about/mission">Mission</Link>
                 </li>
-                <li>
+                <li className="text-center text-lg font-medium">
                   <Link href="/about/founders">Founders</Link>
                 </li>
-                <li>
+                <li className="text-center text-lg font-medium">
                   <Link href="/about/volunteers">Volunteers</Link>
                 </li>
               </ul>
